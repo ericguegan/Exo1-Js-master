dd9a86fe2d95b62afe4748388940a6adb80cd133
ericguegan <eric@w2crea.fr>
2016-09-27 16:53:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+/* votre fonction ici*/
+function showMessage(){
+            alert('Aïe, arrêtez de cliquer !!');
+        }
+/************************* Démarrage du code ****************************/
+
+
+
+	/* modification du style */
+ 
+
+    	function changerStyle(){
+
+	document.getElementById('parag1').style.color = '#fff';
+	document.getElementById('parag1').style.background = '#000';
+	document.getElementById('parag1').style.border = "10px dotted red";
+	document.getElementById('parag1').style.padding="5px";
+	document.getElementById('bod').style.background = 'yellow';
+ }
+
+
+
+	/* Création de la page et son style 
+ 		function creerPage(){
+
+
+ 			var span = document.createElement('span');
+			span.textContent = "Hello!";
+ 		}
+
+
+
+
+ 				nouveauP = document.createElement("<p id='parag1'></p>");
+ 			document.createElement(<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat.</p>);
+
+ 			
+
+*/
+
